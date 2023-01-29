9baf82cf02cf867261d795d0070a785fd3cf25a7
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2023-01-29 08:40:32 -0800

    Adding SOlutions

---
+++ 2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in ascending order using the
+ * selection sort algorithm
+ */
+
+void selection_sort(int *array, size_t size)
+{
+        size_t i, j;
+        int min_pos, numb, check;
+
+        for (i = 0; i < size; i++)
+        {
+                check = 0;
+	      min_pos = i;
+	      for (j = i; j < size; j++)
+	      {
+	              if (array[j] < array[min_pos])
+		    {
+			min_pos = j;
+		         	check = 1;
+		    }
+	      }
+	      if (check == 1)
+	      {
+        	              numb = array[i];
+		    array[i] = array[min_pos];
+		    array[min_pos] = numb;
+		    print_array(array, size);
+	      }
+        }
+}
